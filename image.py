7e3aa40138e966caee984cf62696db194bb8edf1
EvitanRelta <shauntanzongzhi@gmail.com>
2023-01-14 17:58:18 +0800

    Update image command

--- image.py
+++ image.py
@@ -1,7 +1,21 @@
 from PIL import Image
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+import numpy as np
 
 def handle_image(bot):
     @bot.message_handler(commands=['image'])
     def send_image(message):
-        image = Image.new('RGB', (100, 100), color = (73, 109, 137))
-        bot.send_photo(message.chat.id, image)
+        # Create data for the graph
+        x = np.linspace(0, 10, 100)
+        y = np.sin(x)
+
+        # Create the graph
+        plt.plot(x, y)
+
+        # Save the graph to a buffer
+        buf = BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+        bot.send_photo(message.chat.id, buf)
