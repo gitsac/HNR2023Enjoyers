c345c73157aadc3848474be7f84d0c8b537943ca
EvitanRelta <shauntanzongzhi@gmail.com>
2023-01-14 20:08:05 +0800

    Update owe & payfor commands

--- owe.py
+++ owe.py
@@ -1,13 +1,22 @@
 from init_bot import bot
+from finDb import addOweTransaction, userExists
 
 @bot.message_handler(commands=['owe'], chat_types=['group'])
 def handle(message):
     payload = message.text.split()[1:]
     if len(payload) != 2:
         return bot.reply_to(message, f"Expected 2 arguments, got {len(payload)}.")
+    user = message.from_user.username
     recipient = payload[0][1:]
     amount = float(payload[1])
+    
+    if (not userExists(user)):
+        return bot.reply_to(message, f"@{user} is not registered")
+    if (not userExists(recipient)):
+        return bot.reply_to(message, f"@{recipient} is not registered")
+    
+    addOweTransaction(user, recipient, amount)
     bot.reply_to(
         message, 
-        f"@{message.from_user.username} owes @{recipient} ${amount:.2f}"
+        f"@{user} owes @{recipient} ${amount:.2f}"
     )
